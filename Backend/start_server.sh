52ccbc15ce5382bc0458aa75c35b959c52b9295f
Shasuvathanan <shasu1pm@gmail.com>
2026-02-05 03:08:07 +0530

    Initial commit

--- Backend/start_server.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "Installing dependencies..."
-pip install -r requirements.txt
-
-echo ""
-echo "Starting Whisper Transcription Server..."
-echo "Server will be available at http://localhost:8000"
-echo ""
-python server.py
